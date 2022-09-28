7eb5081407d912d24b73a1641f728eeb309fc9e0
Fatima <fzms90@gmail.com>
2022-09-28 15:07:54 +0100

    test5

--- addTheFilm.php
+++ addTheFilm.php
@@ -29,7 +29,7 @@ $distributors = extract_distributor_from_db($db);
     <a name="newFilm"> </a>
     <div class = "userForm">
         <h1 id="form">Add the film to the film collection  </h1>
-        <form class="form"   method="POST" action="addToDb.php">
+        <form class="form" method="POST" action="addToDb.php">
             <label for="title">Title:</label>
             <input type="text" class="title" name="title" placeholder="e.g. Dune" required>
             <label for="director">Director:</label>
@@ -42,14 +42,14 @@ $distributors = extract_distributor_from_db($db);
             <select id="genre" name="genre" required>
                  <?php foreach($genres as $genre){
                     echo '<option value="' . $genre['id'] . '">' . $genre['genre1'] . '</option>';
-                    }; ?>
+                 }; ?>
             </select>
 
             <label for="language">Language:</label>
             <select id="language" name="language" required>
                 <?php foreach($languages as $language){
                     echo '<option value="' . $language['id'] . '">' . $language['original_language1'] . '</option>';
-                    }; ?>
+                }; ?>
             </select>
 
             <label for="distributor">Distributor:</label>
@@ -59,7 +59,7 @@ $distributors = extract_distributor_from_db($db);
                 }; ?>
             </select>
 
-             <label for="release_date_theatres">Release date (theatres):</label>
+            <label for="release_date_theatres">Release date (theatres):</label>
             <input type="text" class="release_date_theatres" name="release_date_theatres" placeholder="-" required>
             <label for="release_date_streaming">Release date (streaming):</label>
             <input type="text" class="release_date_streaming" name="release_date_streaming" placeholder="-" required>
@@ -67,7 +67,6 @@ $distributors = extract_distributor_from_db($db);
             <input type="text" class="runtime" name="runtime" placeholder="-" required>
             <input type="submit" class="submit">
         </form>
-
 </main>
 <footer class="goToTop">
     <a href="#newFilm"> <img src="images/video.png"  alt="" class="home"> </a>
